4a03d76fada20700f6a730da276b7f00af3ed993
wolder3 <walther.cza@outlook.com>
2020-04-17 10:35:26 -0500

    Actualizacion

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ const sequelize = new Sequelize(`${NAME_DB}`, `${USER_DB}`, `${PASS_DB}`, {
   dialect: `${DIALECT_DB}`,
 });
 
-var server = app.listen(port);
+var server = app.listen(port, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 sequelize
   .authenticate()
